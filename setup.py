98dcbd5458c9224737cead8f084b66475f097999
Artemka4433 <artemiicherepaha92@gmail.com>
2020-09-17 11:15:26 +0300

    dfg

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
+#
 def quick_resp(responce):
     listMessage = responce['result']
     global size
